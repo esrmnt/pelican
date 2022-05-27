debf717e89759624d58ae9d44cc68cf0985270bd
Esrmnt <nitish.er@gmail.com>
2022-05-28 02:15:02 +0530

    Added configuration for console as startup project

--- src/esrmnt.pelican.console/Program.cs
+++ src/esrmnt.pelican.console/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             // The other part of the client code constructs the actual chain.
-            
+            System.Console.WriteLine("And now we are here");
         }
     }
 }
