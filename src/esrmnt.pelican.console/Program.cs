6e719d2462cc2eaf5bff2e819474c9e93758d9aa
Esrmnt <nitish.er@gmail.com>
2022-05-28 01:24:08 +0530

    Updated solution

---
+++ src/esrmnt.pelican.console/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace esrmnt.pelican.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The other part of the client code constructs the actual chain.
+            
+        }
+    }
+}
